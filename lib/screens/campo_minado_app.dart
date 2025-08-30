3153bc4d86118424774a880eb42a693d8572ec0f
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2025-08-30 10:34:13 -0300

    adicionado lógica do campo minado

--- lib/screens/campo_minado_app.dart
+++ lib/screens/campo_minado_app.dart
@@ -1,29 +1,57 @@
+import 'dart:math';
+
 import 'package:campo_minado/components/campo_widget.dart';
 import 'package:campo_minado/components/resultado_widget.dart';
+import 'package:campo_minado/components/tabuleiro_widget.dart';
+import 'package:campo_minado/exceptions/explosao_exception.dart';
 import 'package:campo_minado/models/campo.dart';
+import 'package:campo_minado/models/tabuleiro.dart';
 import 'package:flutter/material.dart';
 
-class CampoMinadoApp extends StatelessWidget {
+class CampoMinadoApp extends StatefulWidget {
   const CampoMinadoApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    final campo = Campo(
-      linha: 0,
-      coluna: 0,
-    );
+  State<CampoMinadoApp> createState() => _CampoMinadoAppState();
+}
 
+class _CampoMinadoAppState extends State<CampoMinadoApp> {
+  bool? _venceu;
+  Tabuleiro? _tabuleiro;
+
+  Tabuleiro _getTabuleiro(double largura, double altura) {
+    if (_tabuleiro == null) {
+      int qtdColunas = 15;
+      double tamanhoCampo = largura / qtdColunas;
+      int qtdLinhas = (altura / tamanhoCampo).floor();
+
+      _tabuleiro = Tabuleiro(
+        linhas: qtdLinhas,
+        colunas: qtdColunas,
+        qtdBombas: Random().nextInt(5),
+      );
+    }
+
+    return _tabuleiro!;
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: ResultadoWidget(
-          venceu: true,
+          venceu: _venceu,
           quandoReiniciar: _reiniciar,
         ),
         body: Container(
-          child: CampoWidget(
-            campo: campo,
-            quandoAbrir: _abrir,
-            quandoAlternarMarcacao: _alternarMarcacao,
+          child: LayoutBuilder(
+            builder: (ctx, cts) {
+              return TabuleiroWidget(
+                tabuleiro: _getTabuleiro(cts.maxWidth, cts.maxWidth),
+                abrir: _abrir,
+                alterarMarcacao: _alternarMarcacao,
+              );
+            },
           ),
         ),
       ),
@@ -31,14 +59,43 @@ class CampoMinadoApp extends StatelessWidget {
   }
 
   void _reiniciar() {
-    print('reiniciar');
+    setState(() {
+      _venceu = null;
+      _tabuleiro!.reiniciar();
+    });
+    setState(() {});
   }
 
   void _abrir(Campo c) {
-    print('abrindo');
+    if (_venceu != null) {
+      return;
+    }
+
+    setState(
+      () {
+        try {
+          c.abrir();
+          if (_tabuleiro!.resolvido) {
+            _venceu = true;
+          }
+        } on ExplosaoException {
+          _venceu = false;
+          _tabuleiro!.revelarBombas();
+        }
+      },
+    );
   }
 
   void _alternarMarcacao(Campo c) {
-    print('abrindo marcação');
+    if (_venceu != null) {
+      return;
+    }
+
+    setState(() {
+      c.alternarMarcacao();
+      if (_tabuleiro!.resolvido) {
+        _venceu = true;
+      }
+    });
   }
 }
